7719de6dd239ea00d301553801d7eb4f2e651c4b
Fabrício Valadares <fabricio.valadares@gmail.com>
2024-03-04 17:51:19 -0300

    Create Test.java
    First commit

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test {
+  int main(String[] args) {
+    String nome;
+    System.out.println("Hello World!!");
+    Scanner input = new Scanner(System.in);
+    System.out.print("Informe seu nome: ");
+    nome = input.nextLine();
+    System.out.println("\n" + nome + "\n");
+  
+    
+  }
+}
